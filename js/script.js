dd948eb36bec67e1302d04494f7aa8690ae91ce9
unknown <gamenumber>
2025-09-24 15:10:17 +0900

    테스트 2

--- js/script.js
+++
@@ -1,6 +0,0 @@
-  const isMobile = window.innerWidth <= 768; // 모바일 화면 판단
-  const mapLink = document.querySelector('.map-container iframe:src');
-
-  if (isMobile) {
-    mapLink.href = "https://m.place.naver.com/place/1191092372/home"; // 모바일 앱용 네이버 지도 링크
-  }
